d9d9f77b4aa828ba307f04e57c82eb177d909524
mgarbowski <mikolaj.garbowski@gmail.com>
2023-06-02 12:48:32 +0200

    display customers

--- src/main/java/org/example/HotelApp.java
+++ src/main/java/org/example/HotelApp.java
@@ -4,14 +4,12 @@ import oracle.jdbc.pool.OracleDataSource;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class HotelApp {
     public static void main(String[] args) {
         try (var conn = getDbConnection()) {
-            System.out.println("Hello");
+            showCustomers(conn);
         } catch (SQLException | ConfigurationException e) {
             e.printStackTrace();
         }
@@ -29,9 +27,7 @@ public class HotelApp {
         var port = properties.getString("jdbc.port");
         var serviceName = properties.getString("jdbc.service.name");
 
-        var connectionString = String.format(
-                "jdbc:oracle:thin:%s/%s@//%s:%s/%s",
-                username, password, host, port, serviceName);
+        var connectionString = String.format("jdbc:oracle:thin:%s/%s@//%s:%s/%s", username, password, host, port, serviceName);
 
         var dataSource = new OracleDataSource();
 
@@ -43,5 +39,19 @@ public class HotelApp {
         System.out.println(meta.getDatabaseProductVersion());
 
         return connection;
+    }
+
+    private static void showCustomers(Connection conn) throws SQLException {
+        PreparedStatement query = conn.prepareStatement("SELECT id, name, surname, email FROM customers");
+        ResultSet customers = query.executeQuery();
+        System.out.println("id\tname\tsurname\temail");
+        while (customers.next()) {
+            System.out.println(
+                    customers.getString(1) + "\t"
+                            + customers.getString(2) + "\t"
+                            + customers.getString(3) + "\t"
+                            + customers.getString(4) + "\t"
+            );
+        }
     }
 }
