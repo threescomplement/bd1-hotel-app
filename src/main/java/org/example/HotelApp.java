7c0a0187b545ff9d95b3eec8e85a6a817ace3f72
mgarbowski <mikolaj.garbowski@gmail.com>
2023-06-02 13:14:30 +0200

    handling customer balance

--- src/main/java/org/example/HotelApp.java
+++ src/main/java/org/example/HotelApp.java
@@ -5,11 +5,14 @@ import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HotelApp {
     public static void main(String[] args) {
         try (var conn = getDbConnection()) {
             showCustomers(conn);
+            showCustomerBalance(conn, 1);
         } catch (SQLException | ConfigurationException e) {
             e.printStackTrace();
         }
@@ -41,9 +44,15 @@ public class HotelApp {
         return connection;
     }
 
+    private static void printSeparator() {
+        System.out.println("------------------------------");
+    }
+
     private static void showCustomers(Connection conn) throws SQLException {
         PreparedStatement query = conn.prepareStatement("SELECT id, name, surname, email FROM customers");
         ResultSet customers = query.executeQuery();
+
+        printSeparator();
         System.out.println("id\tname\tsurname\temail");
         while (customers.next()) {
             System.out.println(
@@ -53,5 +62,36 @@ public class HotelApp {
                             + customers.getString(4) + "\t"
             );
         }
+        printSeparator();
+    }
+
+    private static void showCustomerBalance(Connection conn, int customerId) throws SQLException {
+        var products = getCustomerProducts(conn, customerId);
+        var total = getTotalBalance(products);
+        var formatString = "%-16s\t%10.2f\n";
+        printSeparator();
+        System.out.format("%-16s\t%s\n", "Item", "Amount");
+        products.forEach(product -> System.out.printf(formatString, product.name(), product.price()));
+        System.out.printf("%-16s\t%10.2f\n", "Total:", total);
+        printSeparator();
+    }
+
+    private static List<Product> getCustomerProducts(Connection conn, int customerId) throws SQLException {
+        var query = conn.prepareStatement("SELECT item, amount FROM balance WHERE customer_id = ?");
+        query.setInt(1, customerId);
+        var resultSet = query.executeQuery();
+        var products = new ArrayList<Product>();
+
+        while (resultSet.next()) {
+            products.add(new Product(resultSet.getString(1), resultSet.getFloat(2)));
+        }
+
+        return products;
+    }
+
+    private static float getTotalBalance(List<Product> products) {
+        return products.stream()
+                .map(Product::price)
+                .reduce(0.0f, Float::sum);
     }
 }
---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public record Product(String name, float price) {
+}
