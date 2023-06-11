7838cad712a533eef09336134af43d812bcba4c3
Michał Łuszczek <michluszcz@gmail.com>
2023-06-11 19:32:42 +0200

    Implemented update_customer_contact_info and add_service_to_booking

--- database/scripts/procedures.sql
+++ database/scripts/procedures.sql
@@ -7,14 +7,16 @@
 create or replace procedure change_booking_period(booking_id BOOKINGS.ID%type, start_date Date, end_date DATE)
 AS
     v_given_booking BOOKINGS%rowtype;
-    cursor booking is (Select * from BOOKINGS where ID = booking_id);
+    cursor booking is (Select *
+                       from BOOKINGS
+                       where ID = booking_id);
 Begin
     if (end_date < start_date) then
         raise INVALID_NUMBER;
     End if;
     open booking;
     fetch booking into v_given_booking;
-    if  booking%notfound then
+    if booking%notfound then
         DBMS_OUTPUT.PUT_LINE('Booking not found!');
         return;
     end if;
@@ -29,4 +31,64 @@ Exception
     when
         INVALID_NUMBER then
         DBMS_OUTPUT.PUT_LINE('Invalid dates!');
-end;
+end;/
+
+
+create or replace procedure update_customer_contact_info(customer_id CUSTOMERS.ID%type, email CUSTOMERS.EMAIL%type,
+                                                         p_number CUSTOMERS.PHONE_NUMBER%type)
+AS
+    v_given_customer CUSTOMERS%rowtype;
+    cursor customer is (Select *
+                        from CUSTOMERS
+                        where ID = customer_id);
+Begin
+    fetch customer into v_given_customer;
+    if customer%notfound then
+        DBMS_OUTPUT.PUT_LINE('Customer not found!');
+        return;
+    end if;
+    if p_number < 9 or p_number > 14 then
+        DBMS_OUTPUT.PUT_LINE('Invalid number!');
+        return;
+    end if;
+
+    UPDATE CUSTOMERS
+    set EMAIL        = email,
+        PHONE_NUMBER = p_number
+    where ID = customer_id;
+EXCEPtion
+when others then
+    DBMS_OUTPUT.PUT_LINE('Failed to update customer!');
+end;/
+
+
+
+create or replace procedure add_service_to_booking(service_id SERVICES.ID%type, booking_id BOOKINGS.ID%type)
+AS
+    v_given_service SERVICES%rowtype;
+    v_given_booking BOOKINGS%rowtype;
+    cursor service is (Select *
+                       from SERVICES
+                       where ID = service_id);
+    cursor booking is (Select *
+                       from BOOKINGS
+                       where ID = booking_id);
+begin
+    fetch service into v_given_service;
+    if service%notfound then
+        DBMS_OUTPUT.PUT_LINE('Service not found!');
+        return;
+    end if;
+    fetch booking into v_given_booking;
+    if booking%notfound then
+        DBMS_OUTPUT.PUT_LINE('Booking not found!');
+        return;
+    end if;
+    INSERT into BOOKINGS_SERVICES VALUES (DEFAULT, service_id, booking_id);
+EXCEPTION
+    when others then
+        DBMS_OUTPUT.PUT_LINE('Failed to add service!');
+end;/
+
+
+
